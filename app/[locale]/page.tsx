a0dca71a5ad6e3f26964d85dd74122a98582d460
sunshinerao <sunshine.rao@gmail.com>
2026-04-07 09:07:28 +0800

    fix: hide homepage news section when newsEnabled is off

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,8 +7,12 @@ import { EventsPreviewSection } from "./sections/events-preview";
 import { NewsSection } from "./sections/news";
 import { PartnersSection } from "./sections/partners";
 import { CTASection } from "./sections/cta";
+import { getSystemSettingsForServer } from "@/lib/system-settings";
 
-export default function Home() {
+export default async function Home() {
+  const settings = await getSystemSettingsForServer().catch(() => null);
+  const newsEnabled = settings?.newsEnabled !== false;
+
   return (
     <>
       <HeroSection />
@@ -17,7 +21,7 @@ export default function Home() {
       <TracksSection />
       <SpeakersSection />
       <EventsPreviewSection />
-      <NewsSection />
+      {newsEnabled && <NewsSection />}
       <PartnersSection />
       <CTASection />
     </>
